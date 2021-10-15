a58ed9047352cd4b98ddf4159d98b2c4cd4c47a1
KD8CGH <40500870+rkayakr@users.noreply.github.com>
2021-10-15 19:12:16 +0000

    Add files via upload
    aid in magnetometer alignment

---
+++ align.py
@@ -0,0 +1,32 @@
+'''
+simple program to continuously plot a mag component to aid in magnetometer alignment
+Bob Benedict   KD8CGH  10/15/2921
+script reads latest line from log file and plots a value in Thonny plot window
+expects plain tex log
+uncomment component you want to plot
+click in Thonny bottom window and choose "Show Plotter"
+kill right side windows to expand plot window
+'''
+import os, time
+
+#Set the filename and open the file
+filename = 'KD8CGH-20211015-runmag.log'  #  insert your plain text log file name
+file = open(filename, 'r', encoding = 'utf-8')
+
+#Find the size of the file and move to the end
+st_results = os.stat(filename)
+st_size = st_results[6]
+file.seek(st_size)
+
+while 1 :
+    where = file.tell()
+    line = file.readline()
+    if not line:
+        time.sleep(1)
+        file.seek(where)
+    else:
+        print(float(line[83:89])) # rx
+#        print(float(line[90:97])) # ry
+#        print(float(line[98:104])) # rz
+
+        
