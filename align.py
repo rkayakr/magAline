3649acbd59ad817ff37e2fda39a95366cc15504a
KD8CGH <40500870+rkayakr@users.noreply.github.com>
2021-10-25 18:26:27 -0400

    Add files via upload
    now detects json file and plots

--- align.py
+++ align.py
@@ -2,7 +2,7 @@
 simple program to continuously plot a mag component to aid in magnetometer alignment
 Bob Benedict   KD8CGH  10/15/2921
 script reads latest line from log file and plots a value in Thonny plot window
-expects plain tex log
+set filename to log file log
 uncomment component you want to plot
 click in Thonny bottom window and choose "Show Plotter"
 kill right side windows to expand plot window
@@ -10,7 +10,7 @@ kill right side windows to expand plot window
 import os, time
 
 #Set the filename and open the file
-filename = 'KD8CGH-20211015-runmag.log'  #  insert your plain text log file name
+filename = 'KD8CGH-20211025-runmag.log'  #  insert your plain text log file name
 file = open(filename, 'r', encoding = 'utf-8')
 
 #Find the size of the file and move to the end
@@ -25,8 +25,20 @@ while 1 :
         time.sleep(1)
         file.seek(where)
     else:
-        sline=line.split(",")
-#        print(sline[3]) #  x
-        print(sline[4]) # y
-#        print(sline[5]) # z
+        
+        if line[0] == '{':   # json file
+            ls=line.split(':')
+#            lsl=ls[6]
+#            print(lsl.split(',')[0])  # x            
+            lsl=ls[7]
+            print(lsl.split(',')[0])  # y
+#            lsl=ls[8]
+#            print(lsl.split(',')[0])  # z
+         
+        else:   # plain text file
+            ls1=line.split(',')
+#            print(ls1[3]) # x
+            print(ls1[4]) # y
+#            print(ls1[5]) # z
+
         
